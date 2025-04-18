8618aa697993d13056f7a5427e18eed8f0b5ae75
LSM0321\sm002 <sm00216@naver.com>
2025-04-18 21:31:24 +0900

    메인페이지 link로 변경 및 헤더 변경

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.jsx'
-import Main from './pages/Main.jsx'
 
 createRoot(document.getElementById('root')).render(
 
-    <Main />
+    <App />
 
 )
