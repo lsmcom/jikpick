9704733d63074ee4b6bb3c3e5e625b71ea8247e8
LSM0321\sm002 <sm00216@naver.com>
2025-04-20 21:16:07 +0900

    상품등록 페이지 추가 및 이름 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 import './App.css';
 import Main from './pages/Main';
-import ProductRegistration from './pages/ProductRegistration';
+import ProductUpload from './pages/ProductUpload'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Main />} />
-        <Route path="/register" element={<ProductRegistration />} />
+        <Route path="/register" element={<ProductUpload />} />
       </Routes>
     </BrowserRouter>
   );
