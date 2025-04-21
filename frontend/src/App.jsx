180b7bd0ac87557f04f10975dd70d8b3789bf6fb
hyunji yoo <rhjkorea@naver.com>
2025-04-21 18:57:35 +0900

    Merge: 충돌해결

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,27 @@
-import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+
+// 페이지 컴포넌트
 import Main from './pages/Main';
-import ProductUpload from './pages/ProductUpload'
-import MyPage from './pages/MyPage';
+import ProductUpload from './pages/ProductUpload';
+import ProductOrder from './pages/ProductOrder';
+import OrderSuccess from './pages/OrderSuccess';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/productUpload" element={<ProductUpload />} />
-        <Route path="/myPage" element={<MyPage />} />
+        {/* 기본 루트 접속 시 주문 페이지로 리디렉션 */}
+        <Route path="/" element={<Navigate to="/order" replace />} />
+
+        {/* 각 페이지 라우팅 */}
+        <Route path="/upload" element={<ProductUpload />} />
+        <Route path="/order" element={<ProductOrder />} />
+        <Route path="/order/success" element={<OrderSuccess />} />
+
       </Routes>
     </BrowserRouter>
   );
 }
 
-
-export default App
+export default App;
