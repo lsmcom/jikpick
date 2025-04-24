7ff0047db92387aadcb9c7616c4256832968269a
lsmcom <sm00216@naver.com>
2025-04-24 22:17:16 +0900

    메인사진추가, 스크롤탑 컴포넌트추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,10 +25,15 @@ import ShopLike from './pages/ShopLike';
 import ShopOrder from './pages/ShopOrder';
 
 import ModifyInfo from './pages/ModifyInfo';
+import CategoryPage from './pages/CategoryPage';
+import WithdrawReason from './pages/WithDrawReason';
+import WithdrawConfirm from './pages/WithDrawConfirm';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <BrowserRouter>
+    <ScrollToTop /> 
       <Routes>
         {/* 기본 경로를 /shop-sale로 설정 */}
         <Route path="/" element={<Main/>} /> {/* 새로 추가된 ShopSale 컴포넌트 */}
@@ -55,6 +60,9 @@ function App() {
         <Route path="/myShop" element={<SellerStore/>}/>
         <Route path="/shopLike" element={<ShopLike />}/>
         <Route path="/shopOrder" element={<ShopOrder />}/>
+        <Route path="/category/:main/:sub?" element={<CategoryPage />} />
+        <Route path="/withdrawReason" element={<WithdrawReason />}/>
+        <Route path="/withdrawConfirm" element={<WithdrawConfirm />}/>
       </Routes>
     </BrowserRouter>
   );
